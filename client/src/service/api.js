4a11764c72a5f05f71c830f48074d78fcc6939e7
mss07 <45558382+mss07@users.noreply.github.com>
2025-08-09 11:26:48 +0530

    added render's url in frontend

--- client/src/service/api.js
+++ client/src/service/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 
-const URL = 'http://localhost:8000';
+const URL = 'https://studentapp2-fvhj.onrender.com';
 
 export const authenticateSignup = async (data) => {
     try
